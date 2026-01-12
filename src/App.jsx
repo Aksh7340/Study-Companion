6bb9201bc69ff84338faa02af069bc6aa755c0e7
unknown <dharmichandg45@gmail.com>
2026-01-13 02:55:49 +0530

    Added a new state exam which has exam name(new) and aslo passed to studystepup and dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,28 @@ import { useState } from "react";
 import Auth from "./pages/Auth";
 import StudySetup from "./pages/StudySetup";
 import Dashboard from "./pages/Dashboard"
+import { remainingDays } from "./Logic/studyPlanner";
 
 function App() {
   const [show,setShow]=useState(false)
-  const [currentScreen, setCurrentScreen] = useState("auth");
+
   
   const [subjects, setSubjects] = useState([]);
+  const [examData,setExamData]=useState([])
+  
 
   return (
     <>
 
     {
-      !show&&<StudySetup setSubjects={setSubjects}>
+      !show&&<StudySetup setSubjects={setSubjects} setExamData={setExamData} examData={examData}>
     </StudySetup>
     }
 
-   
+   <button onClick={()=>setShow(true)}>Change</button>
 
     {show&&<Dashboard 
-    subjects={subjects}></Dashboard>
+    subjects={subjects}  examData={examData}></Dashboard>
     }
     </>
   );
