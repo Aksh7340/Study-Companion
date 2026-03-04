3dd2143254c0d37d3a4e90bb64110d5641c6f8bc
unknown <dharmichandg45@gmail.com>
2026-03-04 20:30:51 +0530

    Updated according to the new components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Auth from "./pages/Auth";
 import StudySetup from "./pages/StudySetup";
 import Dashboard from "./pages/Dashboard"
 import { remainingDays } from "./Logic/studyPlanner";
+import ExamList from "./Components/Exams/ExamList";
 
 function App() {
   const [show,setShow]=useState(false)
@@ -16,14 +17,13 @@ function App() {
     <>
 
     {
-      !show&&<StudySetup setSubjects={setSubjects} setExamData={setExamData} examData={examData}>
+      !show&&<StudySetup subjects={subjects} setSubjects={setSubjects} setExamData={setExamData} examData={examData}>
     </StudySetup>
     }
 
    <button onClick={()=>setShow(true)}>Change</button>
 
-    {show&&<Dashboard 
-    subjects={subjects}  examData={examData}></Dashboard>
+    {show&&<Dashboard examData={examData}></Dashboard>
     }
     </>
   );
