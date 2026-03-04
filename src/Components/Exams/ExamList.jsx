c7075764b2c800725e2b76b37a327b90b092a170
unknown <dharmichandg45@gmail.com>
2026-03-04 20:20:41 +0530

    Created a component for the exam list

---
+++ src/Components/Exams/ExamList.jsx
@@ -0,0 +1,28 @@
+import { remainingDays } from "../../Logic/studyPlanner";
+import ExamCard from "./ExamCard";
+
+export default function ExamList({ examData, onSelectExam }) {
+   console.log("Type of onSelectExam ",typeof(onSelectExam));
+   
+  return (
+    <>
+    
+      {examData
+        .slice()
+        .sort((a, b) => remainingDays(a.date) - remainingDays(b.date))
+        .map(exam => {
+          const days = remainingDays(exam.date);
+
+          return (
+            <ExamCard
+              key={exam.examId}
+              exam={exam}
+              remainingDays={days}
+              onClick={() => onSelectExam(exam)}
+            />
+            
+          );
+        })}
+    </>
+  );
+}
