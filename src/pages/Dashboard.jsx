cf8c1b2cbf665ea8f62f1e903aaa2be508a06e9d
unknown <dharmichandg45@gmail.com>
2026-03-04 20:26:41 +0530

    Created State based navigation with ExamList.jsx to show selected exam

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,51 +1,33 @@
-import { calculateSubjectWeight, remainingDays,getTotalWeight,distributeDailyHours } from "../Logic/studyPlanner";
 
-function Dashboard({ examData, subjects }) {
+
+import { useState } from "react";
+import ExamList from "../Components/Exams/ExamList";
+
+export default function Dashboard({ examData }) {
+
+  const [selectedExam, setSelectedExam] = useState(null);
+
   return (
     <div>
       <h2>Dashboard</h2>
 
+      {!selectedExam && (
+        <ExamList
+          examData={examData}
+          onSelectExam={setSelectedExam}
+        />
+      )}
 
-      {examData
-        .slice() // create a copy to avoid mutating state
-        .sort(
-          (a, b) => remainingDays(a.date) - remainingDays(b.date)
-        )
-        .map(exam => (
-        <div key={exam.examId} style={{ marginBottom: "30px" }}>
-          <h3>{exam.examName}</h3>
-          <p>Exam Date: {exam.date}</p>
-          <p>Remaining Days: {remainingDays(exam.date)}</p>
+      {selectedExam && (
+        <div>
+          <button onClick={() => setSelectedExam(null)}>
+            Back
+          </button>
 
-          <table border="1">
-            <thead>
-              <tr>
-                <th>Subject</th>
-                <th>Chapters</th>
-                <th>Difficulty</th>
-                <th>Weight</th>
-                <th>Daily Hours Needed</th>
-              </tr>
-            </thead>
-            <tbody>
-              {subjects
-                .filter(sub => sub.examId === exam.examId)
-                .sort((a, b) => calculateSubjectWeight(b) - calculateSubjectWeight(a))
-                .map(sub => (
-                  <tr key={sub.id}>
-                    <td>{sub.name}</td>
-                    <td>{sub.chapters}</td>
-                    <td>{sub.difficulty}</td>
-                    <td>{calculateSubjectWeight(sub)}</td>
-                    <td>{distributeDailyHours(sub,subjects,exam)}</td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
+          <h3>{selectedExam.examName}</h3>
+          <p>Date: {selectedExam.date}</p>
         </div>
-      ))}
+      )}
     </div>
   );
 }
-
-export default Dashboard;
