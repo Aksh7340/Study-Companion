3d52c0c23c5f20b772d242ddf08c9af37450e1a4
unknown <dharmichandg45@gmail.com>
2026-03-07 21:42:43 +0530

    Added validation for duplicate exams

--- src/pages/StudySetup.jsx
+++ src/pages/StudySetup.jsx
@@ -5,9 +5,6 @@ import "./StudySetup.css";
 function StudySetup({ examData, setExamData, setSubjects,subjects }) {
 
   
-
-
-
   return (
     <>
       <ExamForm setExamData={setExamData} examData={examData}></ExamForm>
